30157fcfb83efca31f1ba95d24beff20a0484391
kammradt <vinicius.kammradt1@gmail.com>
2019-05-31 08:05:28 -0300

    :sparkles: - Creating 'Recipe' model

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Recipe(models.Model):
+    DIFFICULTY_LEVELS = (
+        ('Easy', 'Easy'),
+        ('Medium', 'Medium'),
+        ('Hard', 'Hard'),
+    )
+    name = models.CharField(max_length=120)
+    ingredients = models.CharField(max_length=400)
+    picture = models.FileField()
+    difficulty = models.CharField(choices=DIFFICULTY_LEVELS, max_length=10)
+    preparation_time = models.PositiveIntegerField()
+    preparation_guide = models.TextField()
+
+    def __str__(self):
+        return f'Recipe: {self.name}'
