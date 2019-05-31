d26ba95a76eb746e2e6dd5d31bf74c3f9845c5fb
kammradt <vinicius.kammradt1@gmail.com>
2019-05-31 08:20:26 -0300

    :pencil: - Doc for 'core' urls router

--- api/core/urls.py
+++ api/core/urls.py
@@ -8,3 +8,8 @@ router.register(r'recipes', RecipeViewSet)
 urlpatterns = [
     path('', include(router.urls))
 ]
+
+
+# In the code above, the router class generates the following URL patterns:
+# /recipes/ - Create and Read operations can be performed on this route.
+# /recipes/{id} - Read, Update and Delete operations can be performed on this route.
