f2cac44a3fc57497eb9f1d2388356d144a61e574
kammradt <vinicius.kammradt1@gmail.com>
2019-06-01 00:55:02 -0300

    :sparkles: - RecipeCard.vue component created

--- client/components/RecipeCard.vue
+++ client/components/RecipeCard.vue
@@ -1,38 +1,45 @@
 <template>
-  <div class="card recipe-card">
-    <img :src="recipe.picture" class="card-img-top" />
-    <div class="card-body">
-      <h5 class="card-title">{{ recipe.name }}</h5>
-      <p class="card-text">
-        <strong>Ingredients:</strong> {{ recipe.ingredients }}
-      </p>
-      <div class="action-buttons">
-        <nuxt-link
-          :to="`/recipes/${recipe.id}/`"
-          class="btn btn-sm btn-success"
-        >
-          View
+  <v-flex xs12 md4 lg3 pa-2>
+    <v-card hover>
+      <v-img :src="recipe.picture" height="200px" />
+      <v-card-title>
+        <p class="font-weight-medium headline">{{ recipe.name }}</p>
+      </v-card-title>
+
+      <v-card-text>
+        <strong>Ingredients: </strong> {{ recipe.ingredients }}
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <nuxt-link :to="`/recipes/${recipe.id}/`" class="pr-2">
+          <v-btn small color="success" v-text="'View'" />
         </nuxt-link>
-        <nuxt-link
-          :to="`/recipes/${recipe.id}/edit/`"
-          class="btn btn-sm btn-primary"
-        >
-          Edit
+
+        <nuxt-link :to="`/recipes/${recipe.id}/edit/`" class="pr-2">
+          <v-btn small color="info" v-text="'Edit'" />
         </nuxt-link>
-        <button class="btn btn-sm btn-danger" @click="onDelete(recipe.id)">
-          Delete
-        </button>
-      </div>
-    </div>
-  </div>
+
+        <v-btn
+          small
+          color="primary"
+          @click="onDelete(recipe.id)"
+          v-text="'Delete'"
+        >
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-flex>
 </template>
+
 <script>
 export default {
   props: ['recipe', 'onDelete']
 }
 </script>
-<style>
-.recipe-card {
-  box-shadow: 0 1rem 1.5rem rgba(0, 0, 0, 0.6);
+
+<style scoped>
+a {
+  text-decoration: none;
 }
 </style>
