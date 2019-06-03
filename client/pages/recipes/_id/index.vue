bdf257927e6ffe4ecaf70f041a698583911ec58e
kammradt <vinicius.kammradt1@gmail.com>
2019-06-03 15:59:48 -0300

    :sparkles: Single recipe page finished
    Now the user is able to visualize a single recipe tha was previously added

--- client/pages/recipes/_id/index.vue
+++ client/pages/recipes/_id/index.vue
@@ -1,5 +1,74 @@
 <template>
-  <h1>
-    index of _id
-  </h1>
+  <v-app>
+    <v-flex pa-1 text-xs-center display-3 v-text="recipe.name" />
+    <v-layout justify-space-around row wrap fill-height>
+      <v-flex text-xs-center xs12 md6 pa-1 pt-5>
+        <img
+          v-if="recipe.picture"
+          style="width: 400px;"
+          :src="recipe.picture"
+        />
+      </v-flex>
+      <v-flex xs12 md6 pa-1 pt-5>
+        <v-form @submit.prevent="submitRecipe">
+          <v-text-field v-model="recipe.name" readonly label="Recipe name" />
+
+          <v-text-field
+            v-model="recipe.ingredients"
+            readonly
+            label="Ingredients"
+          />
+
+          <v-text-field
+            v-model="recipe.difficulty"
+            readonly
+            label="Difficulty"
+          />
+
+          <label
+            v-text="`Preparation time: ${recipe.preparation_time} minutes`"
+          />
+          <v-slider v-model="recipe.preparation_time" max="600" readonly />
+
+          <v-textarea
+            v-model="recipe.preparation_guide"
+            label="Preparation guide"
+            readonly
+            rows="12"
+            no-resize
+          />
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-app>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'View Recipe'
+    }
+  },
+  data() {
+    return {
+      recipe: {
+        name: '',
+        picture: '',
+        ingredients: '',
+        difficulty: '',
+        preparation_time: '',
+        preparation_guide: ''
+      }
+    }
+  },
+  async asyncData({ $axios, params }) {
+    try {
+      const recipe = await $axios.$get(`/recipes/${params.id}`)
+      return { recipe }
+    } catch (e) {
+      return { recipe: [] }
+    }
+  }
+}
+</script>
